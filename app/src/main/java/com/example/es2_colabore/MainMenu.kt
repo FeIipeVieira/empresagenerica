6c8bc6ac86d1fe7da168866f5bd05268fd21f309
FeIipeVieira <fevieira@id.uff.br>
2019-11-22 20:52:27 -0300

    Navigation Drawer finalmente funcionando. Problema: Um fundo verde petróleo cobre todo o app, impedindo a navegação.

--- app/src/main/java/com/example/es2_colabore/MainMenu.kt
+++ app/src/main/java/com/example/es2_colabore/MainMenu.kt
@@ -3,16 +3,45 @@ package com.example.es2_colabore
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.mainmenu_activity.*
 
-class MainMenu : AppCompatActivity() {
+class MainMenu : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-
+    lateinit var homeFragment: HomeFragment
+    lateinit var workFragment: WorkFragment
+    lateinit var schoolFragment: SchoolFragment
+    lateinit var settingFragment: SettingFragment
+    lateinit var timelineFragment: TimelineFragment
+    lateinit var logoutFragment: LogoutFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainmenu_activity)
+
+        setSupportActionBar(toolBar)
+        val actionBar = supportActionBar
+        actionBar?.title = "Navigation Drawer"
+        val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolBar,
+            (R.string.open),
+            (R.string.close)
+        ){
+
+        }
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
 
         btnBusca.setOnClickListener {
             val intent = Intent(this, Busca::class.java)
@@ -25,7 +54,78 @@ class MainMenu : AppCompatActivity() {
             Toast.makeText(this,"Seu perfil.", Toast.LENGTH_SHORT).show()
             startActivity(intent)
         }
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
 
     }
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId){
+            R.id.home -> {
+                homeFragment = HomeFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, homeFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.work -> {
+                workFragment = WorkFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, workFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.school -> {
+                schoolFragment = SchoolFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, schoolFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.timeline -> {
+                timelineFragment = TimelineFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, timelineFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.setting -> {
+                settingFragment = SettingFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, settingFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.logout -> {
+               logoutFragment = LogoutFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, logoutFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    override fun onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else{
+            super.onBackPressed()
+        }
+    }
+
+
+
 
 }
